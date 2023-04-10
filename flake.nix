d138e6496044dd9428a3886629a67c4e4629ac20
weiss <kingbaiing@163.com>
2023-04-10 18:10:06 +0200

    add aarch64-darwin

--- flake.nix
+++ flake.nix
@@ -13,18 +13,19 @@
         "x86_64-linux"
         "i686-linux"
         "x86_64-darwin"
+        "aarch64-darwin"
         "aarch64-linux"
         "armv6l-linux"
         "armv7l-linux"
       ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in
-      {
-        packages = forAllSystems (system: import ./default.nix {
+    in {
+      packages = forAllSystems (system:
+        import ./default.nix {
           pkgs = import nixpkgs {
             inherit system;
             # overlays = [ cargo2nix.overlays.default ];
           };
         });
-      };
+    };
 }
