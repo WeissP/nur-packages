c29280d6ba186fc8c5fdcc1175372a4d871548fb
weiss <kingbaiing@163.com>
2024-12-14 18:21:58 +0100

    update flake.nix to latest template

--- flake.nix
+++ flake.nix
@@ -1,31 +1,28 @@
 {
   description = "My personal NUR repository";
-
-  # inputs = {
-  #   cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
-  #   flake-utils.follows = "cargo2nix/flake-utils";
-  #   nixpkgs.follows = "cargo2nix/nixpkgs";
-  # };
-
-  outputs = { self, nixpkgs, ... }:
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs =
+    { self, nixpkgs }:
     let
       systems = [
         "x86_64-linux"
         "i686-linux"
         "x86_64-darwin"
-        "aarch64-darwin"
         "aarch64-linux"
         "armv6l-linux"
         "armv7l-linux"
       ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in {
-      packages = forAllSystems (system:
+    in
+    {
+      legacyPackages = forAllSystems (
+        system:
         import ./default.nix {
-          pkgs = import nixpkgs {
-            inherit system;
-            # overlays = [ cargo2nix.overlays.default ];
-          };
-        });
+          pkgs = import nixpkgs { inherit system; };
+        }
+      );
+      packages = forAllSystems (
+        system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system}
+      );
     };
 }
